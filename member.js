339980c2229ca30de311d2838267a55b8b76e657
FranciscoAguero247 <154863495+FranciscoAguero247@users.noreply.github.com>
2024-01-12 21:42:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React'];
+  var member = {
+    name: 'Sara',
+    age: 25,
+    skills: skills
+  };
+  // Write code here
+  return member.skills[2];
+}
